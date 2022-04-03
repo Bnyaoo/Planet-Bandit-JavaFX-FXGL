78e575e5654de1685bc18b71b4a64a202538caff
Benny Chao <bchao5@my.bcit.ca>
2022-04-03 12:07:37 -0700

    Add Javadoc comments for LoginApp class

--- src/main/java/ca/bcit/comp2522/termproject/planetbandit/LoginApp.java
+++ src/main/java/ca/bcit/comp2522/termproject/planetbandit/LoginApp.java
@@ -8,22 +8,37 @@ import javafx.stage.Stage;
 
 import java.util.Objects;
 
-
+/**
+ * Represents the login page of the game.
+ *
+ * @author Benny and Prab
+ * @version 2022
+ */
 public class LoginApp extends Application {
     private static final int APP_WIDTH = 1280;
     private static final int APP_HEIGHT = 720;
 
+    /**
+     * Displays the main menu.
+     * @param stage a Stage object
+     */
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(final Stage stage) throws Exception {
 
         System.out.println(getClass());
-    	Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/login_form.fxml")));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(
+                "/login_form.fxml")));
         stage.setTitle("User Login");
         stage.setScene(new Scene(root, APP_WIDTH, APP_HEIGHT));
         stage.show();
     }
-    
-    public static void main(String[] args) {
+
+    /**
+     * Launches the JavaFX application.
+     *
+     * @param args command line arguments
+     */
+    public static void main(final String[] args) {
         launch(args);
     }
 

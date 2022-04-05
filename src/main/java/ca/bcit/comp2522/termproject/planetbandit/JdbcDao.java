388f6ac214a9354dae51a67cb66ed0db971fbdf1
Prabhjeet Bains <pbains61@my.bcit.ca>
2022-04-05 16:57:21 -0700

    Update JdbcDao class comments

--- src/main/java/ca/bcit/comp2522/termproject/planetbandit/JdbcDao.java
+++ src/main/java/ca/bcit/comp2522/termproject/planetbandit/JdbcDao.java
@@ -1,8 +1,17 @@
 package ca.bcit.comp2522.termproject.planetbandit;
 
-import java.sql.*;
-import java.util.Properties;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
+/**
+ * Creates the connection between a MySql database and our game.
+ *
+ * @author Prab and Benny
+ * @version 2022
+ */
 public class JdbcDao {
     // Replace below database url, username and password with your actual database credentials
     private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/planetbandit?useSSL=false"
@@ -11,7 +20,13 @@ public class JdbcDao {
     private static final String DATABASE_PASSWORD = "root";
     private static final String SELECT_QUERY = "SELECT * FROM registration WHERE email_id = ? and password = ?";
 
-    public boolean validate(final String emailId, final String password) throws SQLException {
+    /**
+     * Validates the user's login.
+     * @param emailId a string that represents the user's email
+     * @param password a string that represents the user's password
+     * @return a boolean value that determines if the login is valid or not
+     */
+    public boolean validate(final String emailId, final String password) {
         // Step 1: Establishing a Connection and
         // try-with-resource statement will auto close the connection.
         try (Connection connection = DriverManager
@@ -34,6 +49,10 @@ public class JdbcDao {
         return false;
     }
 
+    /**
+     * Prints the SQL exception error message.
+     * @param ex an SQLException object
+     */
     public static void printSQLException(final SQLException ex) {
         for (Throwable e : ex) {
             if (e instanceof SQLException) {

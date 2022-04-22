cc9415f74e24b2aa2f6297dfada240e4dbd34fd9
Prabhjeet Bains <pbains61@my.bcit.ca>
2022-04-22 01:14:06 -0700

    Add instructions screen

--- src/main/java/ca/bcit/comp2522/termproject/planetbandit/InstructionsScreen.java
+++ src/main/java/ca/bcit/comp2522/termproject/planetbandit/InstructionsScreen.java
@@ -24,25 +24,19 @@ import javafx.scene.Scene;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-
 /**
  * Represents the main menu of the game.
  *
  * @author Prab and Benny
  * @version 2022
  */
-public class MainMenu extends Application {
+public class InstructionsScreen extends Application {
     private static final int APP_WIDTH = 1280;
     private static final int APP_HEIGHT = 720;
-    private static final int TITLE_X_COORDINATE = 300;
-    private static final int TITLE_Y_COORDINATE = 100;
-    private static final int OPTIONS_X_COORDINATE = 545;
-    private static final int OPTIONS_Y_COORDINATE = 350;
-    private static final int OPTIONS_SIZE = 3;
-    private static final int RECTANGLE_WIDTH = 700;
-    private static final int RECTANGLE_HEIGHT = 60;
-    private static final int RECTANGLE_STROKE = 2;
-    private static final int TITLE_FONT_SIZE = 50;
+
+    private static final int OPTIONS_X_COORDINATE = 50;
+    private static final int OPTIONS_Y_COORDINATE = 50;
+    private static final int OPTIONS_SIZE = 1;
     private static final int SEPARATOR_LENGTH = 210;
     private static final int BUTTON_WIDTH = 200;
     private static final int BUTTON_HEIGHT = 30;
@@ -57,7 +51,7 @@ public class MainMenu extends Application {
 
         root.setPrefSize(APP_WIDTH, APP_HEIGHT);
 
-        try (InputStream is = Files.newInputStream(Paths.get("resources/space.jpg"))) {
+        try (InputStream is = Files.newInputStream(Paths.get("resources/instructions.jpg"))) {
             ImageView img = new ImageView(new Image(is));
             img.setFitWidth(APP_WIDTH);
             img.setFitHeight(APP_HEIGHT);
@@ -66,19 +60,14 @@ public class MainMenu extends Application {
             System.out.println("Couldn't load image");
         }
 
-        Title title = new Title("P L A N E T   B A N D I T");
-        title.setTranslateX(TITLE_X_COORDINATE);
-        title.setTranslateY(TITLE_Y_COORDINATE);
-
         MenuBox vbox = new MenuBox(
-                new MenuItem("PLAY GAME"),
-                new MenuItem("HOW TO PLAY"));
+                new MenuItem("BACK"));
 
         vbox.setTranslateX(OPTIONS_X_COORDINATE);
         vbox.setTranslateY(OPTIONS_Y_COORDINATE);
         vbox.setScaleY(OPTIONS_SIZE);
 
-        root.getChildren().addAll(title, vbox);
+        root.getChildren().addAll(vbox);
 
         return root;
 
@@ -95,26 +84,6 @@ public class MainMenu extends Application {
         primaryStage.setTitle("Planet Bandit");
         primaryStage.setScene(scene);
         primaryStage.show();
-    }
-
-    private static final class Title extends StackPane {
-        /**
-         * Constructs a title for the main menu.
-         * @param name a String that represents the title of the main menu
-         */
-        private Title(final String name) {
-            Rectangle bg = new Rectangle(RECTANGLE_WIDTH, RECTANGLE_HEIGHT);
-            bg.setStroke(Color.WHITE);
-            bg.setStrokeWidth(RECTANGLE_STROKE);
-            bg.setFill(null);
-
-            Text text = new Text(name);
-            text.setFill(Color.WHITE);
-            text.setFont(Font.font("Times New Roman", FontWeight.SEMI_BOLD, TITLE_FONT_SIZE));
-
-            setAlignment(Pos.CENTER);
-            getChildren().addAll(bg, text);
-        }
     }
 
     private static final class MenuBox extends VBox {
@@ -143,8 +112,8 @@ public class MainMenu extends Application {
     private static final class MenuItem extends StackPane {
         private MenuItem(final String name) {
             LinearGradient gradient = new LinearGradient(0, 0, 1, 0, true, CycleMethod.NO_CYCLE,
-                    new Stop(0, Color.YELLOW),
-                    new Stop(1, Color.YELLOW));
+                    new Stop(0, Color.RED),
+                    new Stop(1, Color.RED));
 
             Rectangle bg = new Rectangle(BUTTON_WIDTH, BUTTON_HEIGHT);
             bg.setOpacity(BUTTON_OPACITY);
@@ -152,19 +121,12 @@ public class MainMenu extends Application {
             Text text = new Text(name);
             text.setFill(Color.DARKGREY);
             text.setFont(Font.font("Times New Roman", FontWeight.SEMI_BOLD, BUTTON_FONT_SIZE));
-
+            bg.setFill(Color.RED);
             setAlignment(Pos.CENTER);
             getChildren().addAll(bg, text);
-            setOnMouseEntered(event -> {
-                bg.setFill(gradient);
-                text.setFill(Color.WHITE);
+            setOnMouseEntered(event -> text.setFill(Color.WHITE));
 
-            });
-
-            setOnMouseExited(event -> {
-                bg.setFill(Color.BLACK);
-                text.setFill(Color.DARKGREY);
-            });
+            setOnMouseExited(event -> text.setFill(Color.DARKGREY));
             setOnMousePressed(event -> {
                 text.setFill(Color.BLACK);
                 bg.setFill(Color.YELLOW);
@@ -186,15 +148,10 @@ public class MainMenu extends Application {
     public static void processButtonClick(final String id) {
         GamePlay gamePlay = new GamePlay();
         if (id.equals("1")) {
-            Space space = new Space();
+            MainMenu menu = new MainMenu();
             stage.close();
-            space.start(gamePlay.getStage());
-        }
-
-        if (id.equals("2")) {
-            InstructionsScreen instructionsScreen = new InstructionsScreen();
-            stage.close();
-            instructionsScreen.start(gamePlay.getStage());
+            menu.start(gamePlay.getStage());
         }
     }
 }
+
